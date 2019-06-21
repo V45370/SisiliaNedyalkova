ee599068d2f5c7a4fa6751a627c087e9d01f9d2d
V45370 <vassilbb@gmail.com>
2019-06-21 17:01:42 +0300

    Added lightbox. Changed layout.

--- js/script.js
+++ js/script.js
@@ -1,56 +1,11 @@
- // MDB Lightbox Init
-        $(function () {
-            $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
-        });
-
         // Adding animations to the sections
         $("section").addClass("wow fadeIn");
 
         // Animations Init
         new WOW().init();
 
-        // Open the Modal
-function openModal() {
-    debugger;
-    document.getElementById("myModal").style.display = "block";
-  }
-  
-  // Close the Modal
-  function closeModal() {
-    debugger;
-    document.getElementById("myModal").style.display = "none";
-  }
-  
-  var slideIndex = 1;
-  showSlides(slideIndex);
-  
-  // Next/previous controls
-  function plusSlides(n) {
-    debugger;
-    showSlides(slideIndex += n);
-  }
-  
-  // Thumbnail image controls
-  function currentSlide(n) {
-    debugger;
-    showSlides(slideIndex = n);
-  }
-  
-  function showSlides(n) {
-      debugger;
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    //var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    // for (i = 0; i < dots.length; i++) {
-    //   dots[i].className = dots[i].className.replace(" active", "");
-    // }
-    slides[slideIndex-1].style.display = "block";
-    // dots[slideIndex-1].className += " active";
-    // captionText.innerHTML = dots[slideIndex-1].alt;
-  }
+//Init Lightbox plugin
+        $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+          event.preventDefault();
+          $(this).ekkoLightbox();
+      });
